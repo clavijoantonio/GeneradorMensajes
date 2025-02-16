badfe51bad2bf0acca1024df1cb6b9c444a18c6f
antonio clavijo <clavijo1785@gmail.com>
2025-02-16 00:00:29 -0500

    se agrega proyecto

---
+++ src/main/java/com/Antonio/GeneracionMensajes/GeneracionMensajesApplication.java
@@ -0,0 +1,13 @@
+package com.Antonio.GeneracionMensajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneracionMensajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneracionMensajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Antonio/GeneracionMensajes/GeneracionMensajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Antonio.GeneracionMensajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneracionMensajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
